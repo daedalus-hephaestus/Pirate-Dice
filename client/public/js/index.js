7419ff3c89c4819f3f771339d52f9efd4e731018
J. Gresham Callender <daedalus.hephaestus@gmail.com>
2023-08-07 12:11:45 -0700

    added inputs

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -3,6 +3,9 @@ let cookie = getCookie('auth_cookie');
 if (cookie) SOCKET.emit('auth-cookie', cookie);
 
 let assets = {};
+let mainMenu = {};
+let newClick = true;
+let scene = 'menu';
 
 SOCKET.on('assets', (data) => {
     let types = Object.keys(data);
@@ -64,6 +67,11 @@ SOCKET.on('invalid-bet', () => {
 });
 
 function sketch(p) {
+
+    let scenes = {
+        menu: menuScene
+    };
+
     p.preload = function () {
         for (let t in assets) {
             for (let a in assets[t]) {
@@ -71,6 +79,7 @@ function sketch(p) {
                 if (t == 'images') new ImageClass(a, assets[t][a], p);
             }
         }
+
     };
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight);
@@ -79,21 +88,43 @@ function sketch(p) {
                 assets[t][a].loadFrames();
             }
         }
+        mainMenu.play = new ButtonClass('play', () => {
+            console.log('click')
+            SOCKET.emit('play');
+        }, p);
+        mainMenu.login = new InputClass('text', 'login', 'input', 10, /[A-Za-z]/, p);
     };
     p.draw = function () {
         p.background(0, 0, 0);
         p.noSmooth();
 
         let scale = p.windowWidth / assets.images.table.w;
+        let center = {
+            x: assets.images.table.w / 2,
+            y: p.windowHeight / 2
+        }
         assets.images.table.draw(0, 0, scale);
         assets.animations.candle_12.draw(34, 16, 1, 'candle1', scale, true);
         assets.animations.candle_12.draw(80, 10, 1.5, 'candle2', scale, true);
         assets.animations.candle_12.draw(280, 90, 1.25, 'candle3', scale, true);
         assets.animations.candle_12.draw(210, 70, 1.1, 'candle4', scale, true);
+
+        scenes[scene](scale, center);
     };
+
+    function menuScene(scale, center) {
+        mainMenu.play.draw(center.x - mainMenu.play.w / 2, 15, scale);
+        mainMenu.login.draw(center.x - mainMenu.login.w / 2, 50, scale);
+    }
 
     p.windowResized = function () {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
+    }
+
+    p.mousePressed = function () {
+    };
+    p.mouseReleased = function () {
+        newClick = true;
     }
 }
 
@@ -117,7 +148,7 @@ class AnimationClass {
     constructor(name, path, context) {
         this.path = path; // the animation image path
         this.name = name; // the animation name
-        this.context = context; // the p5 context
+        this.c = context; // the p5 context
         this.frameCount = Number(name.split('_').pop()); // the number of frames
         this.image = context.loadImage(this.path); // loads the image
         assets.animations[name] = this; // stores the class to an object
@@ -142,23 +173,87 @@ class AnimationClass {
             this.instances[id] = 0;
 
         let cur = this.frames[Math.floor(this.instances[id] / 10)];
-        this.context.image(cur, x * scale, y * scale, this.fw * scale, this.fh * scale);
+        this.c.image(cur, x * scale, y * scale, this.fw * scale, this.fh * scale);
     }
-};
-
+}
 class ImageClass {
     constructor(name, path, context) {
         this.path = path;
         this.name = name;
-        this.context = context;
+        this.c = context;
         this.image = context.loadImage(this.path);
         assets.images[name] = this;
     }
     draw(x, y, scale = 1) {
-        this.context.image(this.image, x * scale, y * scale, this.w * scale, this.h * scale)
+        this.c.image(this.image, x * scale, y * scale, this.w * scale, this.h * scale)
     }
     loadFrames() {
         this.w = this.image.width;
         this.h = this.image.height;
+    }
+}
+class ButtonClass {
+    constructor(imageName, action, context) {
+        this.imageName = imageName;
+        this.action = action;
+        this.image = assets.images[this.imageName];
+        this.w = this.image.w;
+        this.h = this.image.h;
+        this.c = context;
+    }
+    draw(x, y, scale = 1) {
+        if (this.image) this.image.draw(x, y, scale);
+
+        if (this.hover(x, y, scale) && this.c.mouseIsPressed) {
+            //click(this.action);
+        }
+    }
+    hover(x, y, scale) {
+        let horizontal = this.c.mouseX > x * scale && this.c.mouseX < x * scale + this.w * scale;
+        let vertical = this.c.mouseY > y * scale && this.c.mouseY < (y + this.h) * scale;
+        return horizontal && vertical;
+    }
+}
+class InputClass {
+    constructor(type, placeholder, imageName, limit, regex, context) {
+        this.type = type;
+        this.placeholder = placeholder;
+        this.image = assets.images[imageName];
+        this.w = this.image.w;
+        this.h = this.image.h;
+        this.limit = limit;
+        this.regex = regex;
+        this.c = context;
+        this.id = `${type}_${Math.floor(Math.round() * 10000)}`;
+
+        this.input = document.createElement('input');
+        this.input.maxLength = this.limit;
+        document.getElementById('inputs').appendChild(this.input);
+
+    }
+    draw(x, y, scale = 1) {
+        if (this.image) this.image.draw(x, y, scale);
+        if (this.hover(x, y, scale) && this.c.mouseIsPressed) {
+            click(() => {
+                this.input.focus();
+            });
+        }
+        this.c.fill(255, 255, 255);
+        this.c.noStroke();
+        this.c.textSize(25);
+        this.c.textAlign(this.c.LEFT, this.c.CENTER);
+        this.c.text(`${this.input.value}`, (x + 6)* scale, (y + this.h/2 + 2) * scale);
+    }
+    hover(x, y, scale) {
+        let horizontal = this.c.mouseX > x * scale && this.c.mouseX < x * scale + this.w * scale;
+        let vertical = this.c.mouseY > y * scale && this.c.mouseY < (y + this.h) * scale;
+        return horizontal && vertical;
+    }
+}
+
+function click(action) {
+    if (newClick) {
+        action();
+        newClick = false;
     }
 }
